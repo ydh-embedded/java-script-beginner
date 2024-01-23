26d03419e586a736e7bfed44b3e6f471d491df71
Danny <ydh.postkasten@yahoo.com>
2024-01-23 15:09:25 +0100

    DHE add files for DOM Objects

---
+++ Kap05/lib/js/oop.js
@@ -0,0 +1,12 @@
+class Mensch {
+    /* construktor Methode  */
+    /* um die initialen Werte des Objectes zu setzen */
+    /* in den runden Klammern übergeben wir die parameter */
+    constructor ( vorname , nachname ) {
+        this.vorname = vorname          ;
+        this.nachname = nachname        ;
+
+    /*  das schlüsselwort this bezieht sich auf das Object welches erstellt wird */
+    /*  dadurch kann das immer ein anderes sein  */
+    }
+}
