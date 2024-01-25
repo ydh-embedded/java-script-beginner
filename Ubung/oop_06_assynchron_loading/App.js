2110b0938d416afc292426cb083272ea1acce9d9
Danny <ydh.postkasten@yahoo.com>
2024-01-25 14:17:30 +0100

    DHE add loading szenario

---
+++ Ubung/oop_06_assynchron_loading/App.js
@@ -0,0 +1,72 @@
+    let success = false  ;
+/*  let success = true   ; */
+
+let fisLoading = true   ;
+
+//#region wir legen die function getProducts() an
+
+function getProducts() {
+
+    return new Promise (
+        ( resolve , reject ) => {
+          setTimeout(() => {
+            if(success){
+                resolve(
+                 [
+                     { name: "Milch"  , inStock: 1                    },
+                     { name: "Kaffee" , inStock: 3                    },
+                     { name: "Kuchen" , inStock: 5                    },
+                 ]
+                );
+            } else {
+                reject( "Reject gibt den Fehler aus !" );
+            }
+          }, 1000 );
+    });
+}
+
+
+/*  let search = "Search-Variable aus einem Input Feld"   ; */
+    let search = "Milch"   ;
+
+
+
+
+
+//#endregion
+
+
+
+
+
+
+
+
+//#region wir rufen die function getProducts() auf
+
+/* getProducts().then().catch() */
+   getProducts()
+    .then((products) => {
+        return products.find((product) => product.name === search )   ;
+    })
+    .then((product) => {
+        console.log(product);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    .finally(() => {
+        fisLoading = false ;
+    });
+
+
+
+
+
+//#endregion
+
+
+
+
+
+    
