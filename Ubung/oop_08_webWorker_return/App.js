aca4c77b034477c6e977fdf29727b25be78752cf
Danny <ydh.postkasten@yahoo.com>
2024-01-25 15:05:20 +0100

    DHE add webWorker return
    undefined

---
+++ Ubung/oop_08_webWorker_return/App.js
@@ -0,0 +1,20 @@
+
+
+
+let worker = new Worker("worker.js");
+
+
+
+
+
+function startMyWork() {
+    
+    worker.postMessage([ 1 , 6 , 4 , 8 , 11 , 68 , 43 , 22 , 2 , 8 , 9 , 12 ])
+    
+}
+
+
+document
+    .querySelector('#startWorker')
+    .addEventListener( "click" , startMyWork )
+    ;
