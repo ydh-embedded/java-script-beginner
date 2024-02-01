a71bc91ef9f0ff6223a9534d16f4cbaa2b5510f5
Danny <ydh.postkasten@yahoo.com>
2024-02-01 11:05:38 +0100

    DHE dev Project

---
+++ Ubung/Heinicke - Projektarbeit Holzshop/lib/js/DropDownForm.js
@@ -0,0 +1,27 @@
+
+
+
+
+window.onload = function() {
+	durchlauf();
+	document.getElementsByName("DropDownFeld")[0].onchange = durchlauf;
+	document.getElementById("formForm").action = "test.php" ;
+};
+
+function durchlauf() {
+	var ausgabe = "";
+
+/* 				zeigt eigenschaften des listen objectes an */
+/* 				var liste = document.getElementsByName("auswahl")[0]; */
+				var liste = document.getElementsByName("DropDownFeld")[0];
+
+				for ( i = 0; i < liste.length; i++) {
+					ausgabe += 'index: ' 				+ liste.options[i].index 			+ '<br>'	;
+					ausgabe += 'defaultSelected: ' 		+ liste.options[i].defaultSelected 	+ '<br>';
+					ausgabe += 'selected: ' 			+ liste.options[i].selected 		+ '<br>';
+					ausgabe += 'text: ' 				+ liste.options[i].text 			+ '<br>';
+					ausgabe += 'value: ' 				+ liste.options[i].value 			+ '<br>' 	+ '<hr>';
+				}
+
+				document.getElementById("ausgabe").innerHTML = ausgabe;
+			}
---
+++ Ubung/Heinicke - Projektarbeit Holzshop/lib/js/onFocus.js
@@ -0,0 +1,85 @@
+
+
+
+
+function testeZeichen   ( forminhalt , erlaubteZeichen ){
+    let alleZeichenOK =true ;
+     for ( let i = 0 ; i < forminhalt.lenght ; i++){
+ 
+ 
+ 
+ /*      if (erlaubteZeichen.regExp /[0-9]_\// ){
+             alleZeichenOK = false;
+             break ;
+         } */
+ 
+ 
+ 
+         if (erlaubteZeichen.indexOf (formInhalt.charAt(i)) === -1 ){            /* wir prüfen ob jedes zeichen in der erlaubten liste drinnen steht */
+             alleZeichenOK = false;
+             break ;
+         }
+     }
+     return alleZeichenOK    ;
+ }
+//#region  wir legen eine function checkForm an
+    function checkForm      (event)     {
+        let fehlermeldung = ""  ;
+        const form = document.querySelector("#configPlate");                    /* wir prüfen auf eingabefeld */
+
+        if  ( form.breite.value.trim() === "" ){
+        fehlermeldung += " Bitte geben Sie Ihre Breite ein!                     <br>"   ;           //#region  function checkForm : wir prüfen ob das Feld leer ist
+        }
+        if  ( form.lange.value.trim() === "" ){
+            fehlermeldung += " Bitte geben Sie Ihre Länge ein!                  <br>"   ;
+        }
+
+        //  function checkForm : wir prüfen die Eingabe
+        if  ( ! testeZeichen ( form.breite.value , "123456789-/")){
+            fehlermeldung += "Bitte geben Sie für die Nummer gültige Zeichen ein    <br>"   ;
+        }
+        if  ( ! testeZeichen ( form.lange.value , "123456789-/")){
+            fehlermeldung += "Bitte geben Sie für die Nummer gültige Zeichen ein    <br>"   ;
+        }
+
+        //  function checkForm : wir geben eine FehlerMeldung aus wenn
+        if  ( fehlermeldung !== ""){
+            putStatus(fehlermeldung);
+            event.preventDefault();
+        }   else    {
+            return confirm("Möchten Sie Ihre Eingabe jetzt berechnen ?");
+        }
+
+}
+//#endregion
+//#region //#region function putStatus : nimm den text aus der variable und packe ihn in das DIV ein
+function putStatus      (myText)    {
+    document.querySelector("#status")
+            .innerHTML      = myText;
+}
+
+function clearStatus    ()          {
+    document.querySelector("#status")
+            .innerHTML      = ""    ;
+}
+
+function checkReset     ()          {
+    return confirm("Wollen Sie die Eingabe wirklich zurücksetzen?");
+}
+//#endregion
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#configPlate") ;
+    form.onsubmit           = checkForm     ;                                               /*     wir reagieren auf das submit ereignis */
+    form.onreset            = checkReset    ;                                               /*     wir reagieren auf das reset ereignis */
+
+    form.breite.onfocus  = () => { putStatus('Bitte geben Sie hier die gewünschte Breite ein'       );}
+    form.breite.onblur   = clearStatus ;
+
+    form.lange.onfocus  = () => { putStatus('Bitte geben Sie hier die gewünschte Länge ein'         );}
+    form.lange.onblur   = clearStatus ;
+
+    form.starke.onfocus  = () => { putStatus('Bitte geben Sie hier die gewünschte Stärke ein'       );}
+    form.starke.onblur   = clearStatus ;
+
+} );
